35bc22d5230b4e384d69d3adbefc7ad034d6e3ff
SanaAfshani <afshani.sh@gmail.com>
2026-06-10 00:17:52 +0330

    fix: admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,5 @@
 export const dynamic = 'force-dynamic';
 
-export const dynamic = 'force-dynamic';
-
 import { Suspense } from 'react';
 import { prisma } from '@/shared/lib/prisma';
 import { AdminDashboardClient } from './AdminDashboardClient';
